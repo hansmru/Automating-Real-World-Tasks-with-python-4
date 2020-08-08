3a4a52cb8e944b859122493311d3b72bbaacc639
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-08-08 12:39:47 +0530

    Update week2.py

--- week2.py
+++ week2.py
@@ -17,15 +17,6 @@ for file in folder:
       keycount=keycount+1
     print(f)
     response=requests.post("http://35.239.240.253/feedback/",json=f)
- keycount = 0
-  f={}
-  with open(path+file) as file:
-    for line in file:
-      value=line.strip()
-      f[keys[keycount]]=value
-      keycount=keycount+1
-    print(f)
-    response=requests.post("http://35.239.240.253/feedback/",json=f)
 print(response.request.body)
 print(response.status_code)
 
