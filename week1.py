e30865b31da52e8b41a5a142b9283024824f51cf
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-08-08 11:34:29 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+from PIL import Image
+
+size = (128,128)
+
+for image in os.listdir("."):
+  if not image.endswith(".py") and not image.endswith(".DS_Store"):
+    img= Image.open(image).convert("RGB")
+    img.rotate(270)
+    img.thumbnail(size)
+    new_name=os.path.basename(image)
+    img.save("/opt/icons/{}".format(new_name),"jpeg")
+    img.close()
+   else:
+    continue
